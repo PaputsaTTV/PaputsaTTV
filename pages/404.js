34d8903fc8ba01d4a520b20aaa0faf2fa8046e88
Ethy56 <Ethy56@users.noreply.github.com>
2022-04-03 11:59:58 -0400

    stuff

--- pages/404.js
+++ pages/404.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Error404 () {
     return (
         <div className="w-full h-[90vh] lg:h-[95vh] bg-black flex justify-center items-center">
-            <Link className="text-red-500 text-2xl text-center cursor-pointer" href="/home" passHref>
-                <h1 className="text-red-500 text-2xl text-center cursor-pointer">There seems to be a problem, click me to go to the home page.</h1>
+            <Link className="text-red-700 text-2xl text-center cursor-pointer" href="/home" passHref>
+                <h1 className="text-red-700 text-2xl text-center cursor-pointer">There seems to be a problem, click me to go to the home page.</h1>
             </Link>
         </div>
     )
