15d910bfe570114f24127da1101687f6e48f3e3c
Ethy56 <Ethy56@users.noreply.github.com>
2022-04-22 15:55:37 -0400

    reworked subathon

--- pages/subathon.js
+++ pages/subathon.js
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
+var txt = [
+    "Tier 1 Sub = 5 minutes",
+    "Tier 2 Sub = 10 minutes",
+    "Tier 3 Sub = 15 minutes",
+    "500 bits = 5 minutes",
+    "$5 donation = 5 minutes",
+    "2 hours minimum",
+    "12 hours maximum, if hit 12 hours, uncapped subathon will be planned",
+    "1k subs = order maid outfit",
+    "Stream wide 5k bits = scarab for a week",
+    "Stream wide 10k bits = scarab for a month",
+    "Drinking stream starts at 9 hour mark"
+]
 
 export default function App() {
-    var [x, setX] = useState(0);
-    useEffect(()=>{
-        setTimeout(()=>{
-            var time = document.getElementById("time");
-            var epch = new Date("April 22 2022 13:00:00");
-            var diff = (epch - Date.now())/1000;
-            var days = `${diff/(60*60*24)}`.split(".");
-            diff = diff%(60*60*24);
-            days = days[0];
-            var hours = `${diff/(60*60)}`.split(".");
-            diff = diff%(60*60);
-            hours = hours[0];
-            var minutes = `${diff/(60)}`.split(".");
-            diff = diff%60;
-            minutes = minutes[0];
-            time.innerText = `${days}d ${hours}h ${minutes}m ${diff.toFixed(0)}s`;
-            setX(x+1);
-        }, 1_000);
-    });
     return (
         <div className="w-[100vw] h-[90vh] lg:h-[95vh] bg-black flex flex-col justify-center items-center">
-            <h1 className="text-white text-3xl">Subathon Countdown!</h1>
-            <h1 id="time" className="text-white text-6xl"></h1>
+            <ul className="list-disc text-white text-md">
+                {txt.map(i=>{
+                    return (
+                        <li key={i} className="p-2">
+                            {i}
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
